86433059bb624ebba36b45e0875ce7807983992f
Mohammad Talim <talimmohammad116@gmail.com>
2025-07-02 13:56:35 +0000

    fix: configure webpack to ignore test files in client builds

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: ["ts", "tsx", "js", "jsx"],
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.module.rules.push({
+        test: /\.(test|spec)\.(js|jsx|ts|tsx)$/,
+        loader: "ignore-loader",
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
