264acb37d290f615d4d34df72cff54580511a65d
Mohammad Talim <talimmohammad116@gmail.com>
2025-07-07 07:34:47 +0000

    chore: update site metadata for better social sharing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,28 @@ const mouseMemoirs = Mouse_Memoirs({
 export const metadata: Metadata = {
   title: "WordHunt Game",
   description: "Can you guess the word before it's too late?",
+  openGraph: {
+    title: "WordHunt Game",
+    description:
+      "Can you guess the word before your health runs out? Choose from 6 categories in this blazing-fast game!",
+    url: "https://play-wordhunt.vercel.app",
+    siteName: "WordHunt Game",
+    images: [
+      {
+        url: "https://play-wordhunt.vercel.app/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "WordHunt Game Preview",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "WordHunt Game",
+    description: "Try to guess the hidden word before you lose.",
+    images: ["https://play-wordhunt.vercel.app/og-image.png"],
+  },
 };
 
 const RootLayout = ({
